c32caa35b65e8ad2ea3554fa5a750abfcda32224
Vighnesh-code <vighneshzagade4@gmail.com>
2025-09-17 23:19:04 +0530

    created some server actions & working on signup/login page

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import { createAdminClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
+
+// Helper Function to get users by their email
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", [email])]
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+// Error handling helper function
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+// Helper function for sending OTPs
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const account = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP.");
+  }
+};
+
+const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+  const accountId = await sendEmailOTP(email);
+};
--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -1,8 +1,9 @@
 export const appwriteConfig = {
-  endpointurl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+  endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
   projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!,
   databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
   usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_USERS_ID!,
   filesCollectionId: process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_FILES_ID!,
   fileStorageBucketId: process.env.NEXT_PUBLIC_APPWRITE_FILE_STORAGE_BUCKET_ID!,
+  apiSecretKey: process.env.NEXT_PUBLIC_APPWRITE_API_KEY!,
 };
