ac39a85d18a9700c6e6b4d3b24f4025654c207b2
Vighnesh-code <vighneshzagade4@gmail.com>
2025-11-11 14:42:31 +0530

    Implement separate documents, image, videos section

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -61,7 +61,7 @@ export const uploadFile = async ({
   }
 };
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (currentUser: Models.Document, types: string[]) => {
   const queries = [
     Query.or([
       Query.equal("owner", [currentUser.$id]),
@@ -69,19 +69,19 @@ const createQueries = (currentUser: Models.Document) => {
     ]),
   ];
 
-  // TODO: Search, sorts, limits ...
+  if (types.length > 0) queries.push(Query.equal("type", types));
 
   return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({ types = [] }: GetFilesProps) => {
   const { databases } = await createAdminClient();
 
   try {
     const currentUser = await getCurrentUser();
     if (!currentUser) throw new Error("User Not Found");
 
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types);
 
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
