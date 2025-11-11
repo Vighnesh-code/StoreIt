03a73f13e7dd23213e6752690f5c96370c716c45
Vighnesh-code <vighneshzagade4@gmail.com>
2025-11-11 17:12:46 +0530

    Implemented Sort Functionality with dedicated Sort Dropdown

--- .next/static/chunks/app_(root)_layout_tsx_4eb5cb._.js
+++ .next/static/chunks/app_(root)_layout_tsx_4eb5cb._.js
@@ -6,7 +6,7 @@
   "path": "static/chunks/app_(root)_layout_tsx_4eb5cb._.js",
   "chunks": [
     "static/chunks/_e3f66a._.js",
-    "static/chunks/node_modules_1fccd7._.js"
+    "static/chunks/node_modules_19eb0f._.js"
   ],
   "source": "dynamic"
 });
